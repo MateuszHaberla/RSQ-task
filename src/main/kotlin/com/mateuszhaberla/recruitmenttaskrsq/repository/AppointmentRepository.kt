602cadfaf58b525e1b0ab7afd23c595cad56a2b9
MateuszHaberla <Mateusz.Haberla22@gmail.com>
2020-04-01 01:22:21 +0200

    add findByPatient to AppointmentRepository

--- src/main/kotlin/com/mateuszhaberla/recruitmenttaskrsq/repository/AppointmentRepository.kt
+++ src/main/kotlin/com/mateuszhaberla/recruitmenttaskrsq/repository/AppointmentRepository.kt
@@ -3,8 +3,10 @@ package com.mateuszhaberla.recruitmenttaskrsq.repository
 
 import com.mateuszhaberla.recruitmenttaskrsq.model.Appointment
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
-interface AppointmentRepository : JpaRepository<Appointment,Long>{
+interface AppointmentRepository : JpaRepository<Appointment, Long> {
+    fun findByPatient(@Param("patient") patientId: Long?): MutableList<Appointment>
 }
