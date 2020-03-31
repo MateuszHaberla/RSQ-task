84f1177fbcc4cfd8c993e054577d0e6424757b86
MateuszHaberla <Mateusz.Haberla22@gmail.com>
2020-03-31 21:32:55 +0200

    Initial commit

---
+++ src/main/kotlin/com/mateuszhaberla/recruitmenttaskrsq/RecruitmentTaskApplication.kt
@@ -0,0 +1,11 @@
+package com.mateuszhaberla.recruitmenttaskrsq
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RecruitmentTaskApplication
+
+fun main(args: Array<String>) {
+	runApplication<RecruitmentTaskApplication>(*args)
+}
---
+++ src/test/kotlin/com/mateuszhaberla/recruitmenttaskrsq/RecruitmentTaskApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mateuszhaberla.recruitmenttaskrsq
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RecruitmentTaskApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
