0ab9bbc3460046a4f6f3aff819a3eb2c3b1539b8
MateuszHaberla <Mateusz.Haberla22@gmail.com>
2020-03-31 23:17:40 +0200

    create CRUD controller for Patient

---
+++ src/main/kotlin/com/mateuszhaberla/recruitmenttaskrsq/controller/PatientCrudController.kt
@@ -0,0 +1,58 @@
+package com.mateuszhaberla.recruitmenttaskrsq.controller
+
+import com.mateuszhaberla.recruitmenttaskrsq.model.Patient
+import com.mateuszhaberla.recruitmenttaskrsq.model.PatientDTO
+import com.mateuszhaberla.recruitmenttaskrsq.service.PatientCrudService
+import io.swagger.annotations.ApiOperation
+import lombok.RequiredArgsConstructor
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping(path = ["/v1/api"])
+@RequiredArgsConstructor
+class PatientCrudController @Autowired constructor(val patientCrudService: PatientCrudService){
+
+    @PostMapping("/patients")
+    @ApiOperation(value = "Create Patient")
+    fun create(@RequestBody patient: Patient): ResponseEntity<Patient> {
+        val createdPatient = patientCrudService.create(patient)
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(createdPatient)
+    }
+
+    @GetMapping("/patients/{id}")
+    @ApiOperation(value = "Read Patient")
+    fun read(@PathVariable("id") id: Long): ResponseEntity<Patient?> {
+        return patientCrudService.read(id)
+                .map { ResponseEntity.ok(it) }
+                .orElseGet{ ResponseEntity.status(HttpStatus.NOT_FOUND).build() }
+
+    }
+
+    @PutMapping("/patients")
+    @ApiOperation(value = "Update Patient")
+    fun update(@RequestBody patientDto: PatientDTO): ResponseEntity<Patient> {
+        return patientCrudService.update(patientDto)
+                .map { ResponseEntity.ok(it) }
+                .orElseGet{ ResponseEntity.status(HttpStatus.NOT_FOUND).build() }
+    }
+
+    @DeleteMapping("/patients/{id}")
+    @ApiOperation(value = "Delete Patient")
+    fun delete(@PathVariable("id") id: Long): ResponseEntity<Patient> {
+        return if (patientCrudService.delete(id))
+            ResponseEntity.status(HttpStatus.NO_CONTENT).build()
+        else ResponseEntity.status(HttpStatus.NOT_FOUND).build()
+    }
+}
