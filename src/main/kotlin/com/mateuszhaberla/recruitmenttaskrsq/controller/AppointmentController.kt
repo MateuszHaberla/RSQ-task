f42955a7b20f65d55766b6d69fe11d49e0fb56f2
MateuszHaberla <Mateusz.Haberla22@gmail.com>
2020-04-01 17:52:47 +0200

    add patch endpoint to AppointmentController

--- src/main/kotlin/com/mateuszhaberla/recruitmenttaskrsq/controller/AppointmentController.kt
+++ src/main/kotlin/com/mateuszhaberla/recruitmenttaskrsq/controller/AppointmentController.kt
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
@@ -48,5 +49,14 @@ class AppointmentController @Autowired constructor(
         return if (appointmentCrudService.delete(id))
             ResponseEntity.status(HttpStatus.NO_CONTENT).build()
         else ResponseEntity.status(HttpStatus.NOT_FOUND).build()
+    }
+
+    @PatchMapping("/appointments/{id}")
+    @ApiOperation(value = "Patch Appointments")
+    fun patch(@PathVariable("id") id: Long, @RequestBody appointmentChangesMap: HashMap<String, String>): ResponseEntity<Appointment> {
+        return appointmentCrudService.patch(id, appointmentChangesMap)
+                .map { ResponseEntity.ok(it) }
+                .orElseGet { ResponseEntity.status(HttpStatus.NOT_FOUND).build() }
+
     }
 }
