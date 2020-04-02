99265971e86289d90b50d39973a73c9831a587be
MateuszHaberla <Mateusz.Haberla22@gmail.com>
2020-04-02 02:52:07 +0200

    clean code on Doctor entity

--- src/main/kotlin/com/mateuszhaberla/recruitmenttaskrsq/model/Doctor.kt
+++ src/main/kotlin/com/mateuszhaberla/recruitmenttaskrsq/model/Doctor.kt
@@ -13,9 +13,13 @@ import javax.persistence.Id
 @Entity
 @NoArgsConstructor
 data class Doctor(
-        @Id @GeneratedValue(strategy = GenerationType.AUTO) val id: Long,
-        @Column(nullable = false) val name: String,
-        @Column(nullable = false) val surname: String,
+        @Id @GeneratedValue(strategy = GenerationType.AUTO)
+        val id: Long,
+        @Column(nullable = false)
+        val name: String,
+        @Column(nullable = false)
+        val surname: String,
         @Enumerated(EnumType.STRING)
-        @Column(nullable = false) val specialization: SpecializationEnum
+        @Column(nullable = false)
+        val specialization: Specialization
 )
