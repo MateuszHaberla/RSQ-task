b5a53bc4bface29c568078adf0f8fe4963604155
MateuszHaberla <Mateusz.Haberla22@gmail.com>
2020-04-02 02:49:24 +0200

    add patch method to PatientCrudController

--- src/main/kotlin/com/mateuszhaberla/recruitmenttaskrsq/controller/PatientCrudController.kt
+++ src/main/kotlin/com/mateuszhaberla/recruitmenttaskrsq/controller/PatientCrudController.kt
@@ -1,15 +1,14 @@
 package com.mateuszhaberla.recruitmenttaskrsq.controller
 
+import com.mateuszhaberla.recruitmenttaskrsq.dto.PatientDto
 import com.mateuszhaberla.recruitmenttaskrsq.model.Patient
-import com.mateuszhaberla.recruitmenttaskrsq.model.PatientDTO
 import com.mateuszhaberla.recruitmenttaskrsq.service.PatientCrudService
 import io.swagger.annotations.ApiOperation
-import lombok.RequiredArgsConstructor
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
@@ -19,8 +18,9 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping(path = ["/v1/api"])
-@RequiredArgsConstructor
-class PatientCrudController @Autowired constructor(val patientCrudService: PatientCrudService){
+class PatientCrudController(
+        val patientCrudService: PatientCrudService
+) {
 
     @PostMapping("/patients")
     @ApiOperation(value = "Create Patient")
@@ -36,23 +36,21 @@ class PatientCrudController @Autowired constructor(val patientCrudService: Patie
     fun read(@PathVariable("id") id: Long): ResponseEntity<Patient?> {
         return patientCrudService.read(id)
                 .map { ResponseEntity.ok(it) }
-                .orElseGet{ ResponseEntity.status(HttpStatus.NOT_FOUND).build() }
-
+                .orElseGet { ResponseEntity.status(HttpStatus.NOT_FOUND).build() }
     }
 
     @GetMapping("/patients")
     @ApiOperation(value = "Read All Patients")
-    fun readAll(): List<ResponseEntity<Patient>> {
-        return patientCrudService.readAll()
-                .map { ResponseEntity.ok(it) }
+    fun readAll(): ResponseEntity<List<Patient>> {
+        return ResponseEntity.ok(patientCrudService.readAll())
     }
 
     @PutMapping("/patients")
     @ApiOperation(value = "Update Patient")
-    fun update(@RequestBody patientDto: PatientDTO): ResponseEntity<Patient> {
+    fun update(@RequestBody patientDto: PatientDto): ResponseEntity<Patient> {
         return patientCrudService.update(patientDto)
                 .map { ResponseEntity.ok(it) }
-                .orElseGet{ ResponseEntity.status(HttpStatus.NOT_FOUND).build() }
+                .orElseGet { ResponseEntity.status(HttpStatus.NOT_FOUND).build() }
     }
 
     @DeleteMapping("/patients/{id}")
@@ -61,5 +59,13 @@ class PatientCrudController @Autowired constructor(val patientCrudService: Patie
         return if (patientCrudService.delete(id))
             ResponseEntity.status(HttpStatus.NO_CONTENT).build()
         else ResponseEntity.status(HttpStatus.NOT_FOUND).build()
+    }
+
+    @PatchMapping("/patients/{id}")
+    @ApiOperation(value = "Patch Patient")
+    fun patch(@PathVariable("id") id: Long, @RequestBody patientChangesMap: HashMap<String, String>): ResponseEntity<Patient> {
+        return patientCrudService.patch(id, patientChangesMap)
+                .map { ResponseEntity.ok(it) }
+                .orElseGet { ResponseEntity.status(HttpStatus.NOT_FOUND).build() }
     }
 }
