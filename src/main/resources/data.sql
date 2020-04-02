fca48680c1fdd387f650c89505a9784dcfb79d79
MateuszHaberla <Mateusz.Haberla22@gmail.com>
2020-04-02 02:41:51 +0200

    fix sql queries to fit changed model class

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 --Patient
-INSERT INTO patient (id,name,surname,address) VALUES (1,'Mateusz','Haberla','Poznań')
+INSERT INTO patient (id,name,surname,address) VALUES (1,'Mateusz','Haberla','Wrocław')
 INSERT INTO patient (id,name,surname,address) VALUES (2,'Morty','Smith','Smith Residence')
 INSERT INTO patient (id,name,surname,address) VALUES (3,'Summer','Smith','Smith Residence')
 INSERT INTO patient (id,name,surname,address) VALUES (4,'Beth','Smith','Smith Residence')
@@ -13,8 +13,8 @@ INSERT INTO doctor (id,name,surname,specialization) VALUES (4,'Zbigniew','Religa
 INSERT INTO doctor (id,name,surname,specialization) VALUES (5,'Hipo','Krates','ALL')
 
 --Appointments
-INSERT INTO appointment (id,date,hour,place,doctor,patient) VALUES (1,'2020-05-30T23:03:07.508Z','2020-05-30T23:03:07.508Z','Poznań',1,1)
-INSERT INTO appointment (id,date,hour,place,doctor,patient) VALUES (2,'2020-05-30T23:03:07.508Z','2020-05-30T23:03:07.508Z','Poznań',2,2)
-INSERT INTO appointment (id,date,hour,place,doctor,patient) VALUES (3,'2020-05-30T23:03:07.508Z','2020-05-30T23:03:07.508Z','Poznań',1,3)
-INSERT INTO appointment (id,date,hour,place,doctor,patient) VALUES (4,'2020-05-30T23:03:07.508Z','2020-05-30T23:03:07.508Z','Poznań',2,4)
-INSERT INTO appointment (id,date,hour,place,doctor,patient) VALUES (5,'2020-05-30T23:03:07.508Z','2020-05-30T23:03:07.508Z','Poznań',2,5)
+INSERT INTO appointment (id,time_Of_Appointment,office_Address,doctor_Id,patient_Id) VALUES (1,'2020-05-30T23:03:07.508Z','Poznań',1,1)
+INSERT INTO appointment (id,time_Of_Appointment,office_Address,doctor_Id,patient_Id) VALUES (2,'2020-05-30T23:03:07.508Z','Poznań',2,2)
+INSERT INTO appointment (id,time_Of_Appointment,office_Address,doctor_Id,patient_Id) VALUES (3,'2020-05-30T23:03:07.508Z','Poznań',1,3)
+INSERT INTO appointment (id,time_Of_Appointment,office_Address,doctor_Id,patient_Id) VALUES (4,'2020-05-30T23:03:07.508Z','Poznań',2,4)
+INSERT INTO appointment (id,time_Of_Appointment,office_Address,doctor_Id,patient_Id) VALUES (5,'2020-05-30T23:03:07.508Z','Poznań',2,5)
