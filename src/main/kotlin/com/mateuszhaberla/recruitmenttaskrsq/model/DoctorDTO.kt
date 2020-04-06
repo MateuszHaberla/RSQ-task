a5d5a87e22a2b3599abe8f6e2d37edb3986f5115
MateuszHaberla <Mateusz.Haberla22@gmail.com>
2020-04-06 03:06:12 +0200

    delete unnecessary Dto's from mapper

--- src/main/kotlin/com/mateuszhaberla/recruitmenttaskrsq/model/DoctorDTO.kt
+++
@@ -1,8 +0,0 @@
-package com.mateuszhaberla.recruitmenttaskrsq.model
-
-data class DoctorDTO(
-        val id: Long,
-        val name: String,
-        val surname: String,
-        val specialization: Specialization
-)
