fa0b48a8a6c89fcdf50046f8bdc8b8b73b31229a
Mateusz Haberla <mateusz.haberla@rsqtechnologies.com>
2020-07-09 00:21:24 +0200

    cos

--- src/main/kotlin/com/mateuszhaberla/recruitmenttaskrsq/model/Doctor.kt
+++ src/main/kotlin/com/mateuszhaberla/recruitmenttaskrsq/model/Doctor.kt
@@ -9,17 +9,17 @@ import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
 
-
 @Entity
 @NoArgsConstructor
 data class Doctor(
-        @Id @GeneratedValue(strategy = GenerationType.AUTO)
-        val id: Long,
-        @Column(nullable = false)
-        val name: String,
-        @Column(nullable = false)
-        val surname: String,
-        @Enumerated(EnumType.STRING)
-        @Column(nullable = false)
-        val specialization: Specialization
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    val id: Long,
+    @Column(nullable = false)
+    val name: String,
+    @Column(nullable = false)
+    val surname: String,
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    val specialization: Specialization,
+    val cos: String
 )
